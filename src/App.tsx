94cad4227c81c751d71fd0e39373aeb33c50c510
Charles Cruse <charle9@vt.edu>
2023-08-28 14:15:02 -0400

    example expense list

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,39 @@
-import ListGroup from "./components/ListGroup"
+import { useState } from "react";
+import ListGroup from "./components/ListGroup";
+import ExpenseList from "./expense-tracker/components/ExpenseList";
+import ExpenseFilter from "./expense-tracker/components/ExpenseFilter";
+import ExpenseForm from "./expense-tracker/components/ExpenseForm";
+import categories from "./expense-tracker/categories";
 
 function App() {
+  const [expenses, setExpenses] = useState([
+    { id: 1, description: "aaa", amount: 10, category: "Groceries" },
+    { id: 2, description: "bbb", amount: 10, category: "Utilities" },
+    { id: 3, description: "ccc", amount: 10, category: "Utilities" },
+    { id: 4, description: "ddd", amount: 10, category: "Utilities" },
+  ]);
+  const [selectedCateogry, setSelectedCategory] = useState("All categories");
+  const visibleExpenses =
+    selectedCateogry != "All categories"
+      ? expenses.filter((expense) => expense.category === selectedCateogry)
+      : expenses;
 
   return (
-    <ListGroup />
-  )
+    <div>
+      <div className="mb-5">
+        <ExpenseForm />
+      </div>
+      <div className="mb-3">
+        <ExpenseFilter
+          onSelectCategory={(category) => setSelectedCategory(category)}
+        />
+      </div>
+      <ExpenseList
+        expenses={visibleExpenses}
+        onDelete={(id) => setExpenses(expenses.filter((e) => e.id !== id))}
+      />
+    </div>
+  );
 }
 
-export default App
+export default App;
