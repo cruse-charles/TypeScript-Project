36ae45992efb424b063cea9004a47e5115b09c51
Charles Cruse <charle9@vt.edu>
2023-09-04 14:56:17 -0400

    displayed platform icons

---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,44 @@
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { Platform } from "../hooks/useGames";
+import {
+  FaWindows,
+  FaPlaystation,
+  FaXbox,
+  FaLinux,
+  FaApple,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+
+interface Props {
+  platforms: Platform[];
+}
+
+const PlatformIconList = ({ platforms }: Props) => {
+  const iconMap: { [key: string]: IconType } = {
+    // we use square brackets to do an index signature, saying we don't care about the names here for keys, since we normally do a name: string or something, don't have to specify
+    // IconType is defined in reacticons library
+    //mapping to the slug which is a textual id, good to use it
+    pc: FaWindows,
+    playstation: FaPlaystation,
+    xbox: FaXbox,
+    nintendo: SiNintendo,
+    max: FaApple,
+    linux: FaLinux,
+    andorid: FaAndroid,
+    ios: MdPhoneIphone,
+    web: BsGlobe,
+  };
+
+  return (
+    <HStack marginY={1}>
+      {platforms.map((platform) => (
+        <Icon as={iconMap[platform.slug]} color="gray.500" />
+      ))}
+    </HStack>
+  );
+};
+
+export default PlatformIconList;
