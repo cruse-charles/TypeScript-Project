8dae28c2a2d5e7100c6b3aab27990e77bee8d984
Charles Cruse <charle9@vt.edu>
2023-09-07 02:24:09 -0400

    shifted genres to static data

--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,4 +1,4 @@
-import useData from "./useData";
+import genres from '../data/genres'
 
 export interface Genre {
   id: number;
@@ -6,7 +6,10 @@ export interface Genre {
   image_background: string;
 }
 
+//No longer going to call server for this data, since it basically never changes. So let's render it ourselves since it is static data
+// const useGenres = () => useData<Genre>('/genres');
 
-const useGenres = () => useData<Genre>('/genres');
+//Want to return this object since that is what we've defined already in the past for the other components and don't want to change everything again
+const useGenres = () => ({data: genres, isLoading: false, error: null})
 
 export default useGenres;
